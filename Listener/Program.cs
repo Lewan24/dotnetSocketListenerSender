86ebe6e3f6de3a64648275d9117789a18fbb3e20
artur <artur@kali.kali.local>
2023-06-15 09:05:42 +0200

    Initialize projects. Create Listener. Create Sender. Implement example of usage both projects

---
+++ Listener/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var port = 2245;
+
+while(true)
+    Listen(port);
+
+void Listen(int portToListen)
+{
+    try
+    {
+        // create new socket
+        var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        var ipAddressToListen = IPAddress.Any; // accept any ip address that communicates with selected port
+    
+        var localEndPoint = new IPEndPoint(ipAddressToListen, portToListen);
+        listener.Bind(localEndPoint);
+
+        // listen on socket
+        listener.Listen(10); // set max number of awaiting connections
+
+        Console.WriteLine($"Listening on port {portToListen}...");
+
+        while (true)
+        {
+            // accept connections
+            var clientSocket = listener.Accept();
+
+            // get clients connection ip // from where the connections comes
+            var remoteClientIp = (((IPEndPoint)clientSocket.RemoteEndPoint!)!).Address;
+        
+            // get data as byte[] and convert them to string
+            var buffer = new byte[1024];
+            var bytesRead = clientSocket.Receive(buffer);
+            var receivedData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Data from connection {remoteClientIp}: {receivedData}");
+
+            // -------------------------
+            //     Work on got data
+            // -------------------------
+
+            // close connection
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Exception has been thrown: " + ex.Message);
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string ipAddress = "127.0.0.1"; // ip address where the listener works
+var port = 2245; // port where the listener listens
+
+while (true)
+{
+    Console.WriteLine("Type data that you want to send: ");
+    Console.Write("> ");
+    var userData = Console.ReadLine();
+
+    if (userData?.ToLower() == "exit")
+        break;
+
+    Send(port, ipAddress, userData);
+}
+
+void Send(int serverPort, string serverIp, string? dataToSend)
+{
+    try
+    {
+        // create new socket to communicate with
+        var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // connect with the server
+        var serverIpAddress = IPAddress.Parse(serverIp);
+        var serverEndPoint = new IPEndPoint(serverIpAddress, serverPort);
+        clientSocket.Connect(serverEndPoint);
+
+        if (string.IsNullOrWhiteSpace(dataToSend))
+            dataToSend = "Default value of data to send";
+        
+        // convert data to byte[]
+        var dataBuffer = Encoding.ASCII.GetBytes(dataToSend);
+    
+        clientSocket.Send(dataBuffer);
+
+        Console.WriteLine("Data has been sent.\n");
+    
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception has been thrown: {ex.Message}\n");
+    }
+}
